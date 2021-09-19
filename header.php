3e73e1e5ef3af75b8c5e459baba0cb69f9f82838
Denis <fl.zaiarnyi@gmail.com>
2021-09-19 10:55:55 +0300

    fix: fix path in header.php

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 		<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover">
 		<?php wp_head(); ?>
 	</head>
-	<body>
+	<body <?php body_class(); ?>>
 		<div class="wrapper">
 			<header class="header">
 				<div class="header__container _container">
